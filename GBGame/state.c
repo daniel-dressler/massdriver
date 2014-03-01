c81babcb777bb72eb7d20afd96e9185ef94d47bb
Philip Whittingham <philip.whittingham@gmail.com>
2014-03-01 03:20:46 -0700

    Added State Info

--- GBGame/state.c
+++ GBGame/state.c
@@ -9,9 +9,18 @@ State* GetState()
 
 void init_state()
 {
+	int i;
+
 	g_state.time = 0;
-	g_state.plyr_x = 50;
-	g_state.plyr_y = 50;
+
+	g_state.player1.pos.x = 80;
+	g_state.player1.pos.y = 120;
+
+	for( i = 0; i < MAX_ENEMIES; i++ )
+		g_state.enemies[i].active = 0;
+
+	for( i = 0; i < MAX_BULLETS; i++ )
+		g_state.bullets[i].active = 0;
 }
 
 void tick_state()
